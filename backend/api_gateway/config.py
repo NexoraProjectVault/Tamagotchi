4de245dc01dfe846b3f2282d814e626ce4f2083e
Richelle Pereira <richelle0001@gmail.com>
2025-11-10 21:14:41 -0500

    Fixed the config for api_gateway.

--- backend/api_gateway/config.py
+++ backend/api_gateway/config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 class Config:
     DEBUG = os.getenv('DEBUG', 'False') == 'True'
-    USER_SERVICE_URL = os.getenv('USER_SERVICE_URL', 'http://user_service:5001')
-    PET_SERVICE_URL = os.getenv('PET_SERVICE_URL', 'http://pet_service:5002')
-    TASK_SERVICE_URL = os.getenv('TASK_SERVICE_URL', 'http://task_service:5003')
+    USER_SERVICE_URL = os.getenv('USER_SERVICE_URL')
+    PET_SERVICE_URL = os.getenv('PET_SERVICE_URL')
+    TASK_SERVICE_URL = os.getenv('TASK_SERVICE_URL')
