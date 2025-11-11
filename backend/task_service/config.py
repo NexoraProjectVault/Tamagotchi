1857f6280e829c8abfcf52ebbb41fe7de1ff6f36
Richelle Pereira <richelle0001@gmail.com>
2025-11-10 20:45:50 -0500

    Fixed the config for task.

--- backend/task_service/config.py
+++ backend/task_service/config.py
@@ -5,9 +5,6 @@ load_dotenv()
 
 class Config:
     DEBUG = os.getenv('DEBUG', 'False') == 'True'
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'TASK_DATABASE_URL',
-        'postgresql://postgres:postgres@task_db:5432/task_service'
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv('TASK_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = os.getenv('DEBUG', 'False') == 'True'
+    SQLALCHEMY_ECHO = DEBUG
