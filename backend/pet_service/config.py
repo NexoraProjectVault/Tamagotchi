091e1f6810b80a61d2e466ade99f0895c99c4188
Richelle Pereira <richelle0001@gmail.com>
2025-11-10 23:19:04 -0500

    Updated the config file of pet_service.

--- backend/pet_service/config.py
+++ backend/pet_service/config.py
@@ -5,9 +5,6 @@ load_dotenv()
 
 class Config:
     DEBUG = os.getenv('DEBUG', 'False') == 'True'
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'USER_DATABASE_URL',
-        'postgresql://postgres:postgres@pet_db:5432/pet_service'
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv('PET_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = os.getenv('DEBUG', 'False') == 'True'
