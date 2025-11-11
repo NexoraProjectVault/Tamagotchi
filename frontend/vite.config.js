71399051b13ed821720693e7b93c4f20782311ca
Richelle Pereira <richelle0001@gmail.com>
2025-11-10 19:44:24 -0500

    Updated the vite config file.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(
-      process.env.VITE_API_URL || 'http://localhost:5173'
-    )
-  },
   server: {
     middlewareMode: false,
-    allowedHosts: ['tamagotchi-69fg.onrender.com'],
+    allowedHosts: ['tamagotchi-n0y6.onrender.com'],
   },
 })
