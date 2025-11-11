10ddafbd6f4df456b755cc4e8499ec274dfd0156
Richelle Pereira <richelle0001@gmail.com>
2025-11-10 21:25:51 -0500

    Fixed the config for user service.

--- backend/user_service/config.py
+++ backend/user_service/config.py
@@ -4,15 +4,12 @@ load_dotenv()
 
 class Config:
     DEBUG = os.getenv('DEBUG', 'False') == 'True'
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'USER_DATABASE_URL',
-        'postgresql://postgres:postgres@user_db:5432/user_service'
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv('USER_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = os.getenv('DEBUG', 'False') == 'True'
     
     # >>> For configuring jwt user login <<<
-    JWT_SECRET = os.getenv('JWT_SECRET', 'dev-only-change-me')
-    JWT_ISS = os.getenv('JWT_ISS', 'pixelnova-user')
-    JWT_AUD = os.getenv('JWT_AUD', 'pixelnova-clients')
-    JWT_ACCESS_TTL_MIN = int(os.getenv('JWT_ACCESS_TTL_MIN', '15'))
+    JWT_SECRET = os.getenv('JWT_SECRET')
+    JWT_ISS = os.getenv('JWT_ISS')
+    JWT_AUD = os.getenv('JWT_AUD')
+    JWT_ACCESS_TTL_MIN = int(os.getenv('JWT_ACCESS_TTL_MIN'))
